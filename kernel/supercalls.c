8d48e10871da68109880663155fe0575abcb8b9a
pershoot <190600+pershoot@users.noreply.github.com>
2025-12-25 21:05:17 -0500

    kernel: Allow building when KSU_SUSFS is unset

--- kernel/supercalls.c
+++ kernel/supercalls.c
@@ -466,7 +466,13 @@ static int do_get_hook_mode(void __user *arg)
 {
 	struct ksu_get_hook_mode_cmd cmd = {0};
 
+#ifndef CONFIG_KSU_SUSFS
+	strscpy(cmd.mode, "Kprobes", sizeof(cmd.mode));
+#elif defined(CONFIG_KPROBES) || defined(CONFIG_TRACEPOINTS)
+	strscpy(cmd.mode, "Hybrid", sizeof(cmd.mode));
+#else
 	strscpy(cmd.mode, "Inline", sizeof(cmd.mode));
+#endif // CONFIG_KSU_SUSFS
 
 	if (copy_to_user(arg, &cmd, sizeof(cmd))) {
 		pr_err("get_hook_mode: copy_to_user failed\n");
